de9003fcd38093b91b05c45db84ce261f4118665
Antti Koivumäki <anttko@ety-a2123b-31.edu.wks.metropolia.fi>
2018-01-25 15:39:02 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
